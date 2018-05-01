70d3c771987940fa0e4e9c7864b76818e7052d6f
Mikel Matticoli <mamatticoli@wpi.edu>
2018-04-30 22:39:59 -0400

    demo code

---
+++ Assets/CameraThing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraThing : MonoBehaviour {
+
+	public GameObject follow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector3(follow.transform.position.x, gameObject.transform.position.y, follow.transform.position.z);
+	}
+}
